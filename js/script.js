df9e27be7a93a2a9fb23e37df16792bbead018bf
KevinSonda <kevinsonda@gmail.com>
2024-07-04 01:27:50 +0200

    [UPD]verifico se il numero e un multiplo di 3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 //creao un for che mi stampa i numeri da 1 a 100
 for (let i =1; i <=100; i++) {
+    // verifico se il numero attualmente iterato e un multiplo di 3
+    if (i % 3 == 0 && i % 5 !=0){
+        console.log('Fizz');
+    }
     
 }
